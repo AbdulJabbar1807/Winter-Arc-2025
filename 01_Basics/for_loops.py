46f3b36e01a8e9bc8b4b97ac523487a19ad5c977
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2025-12-28 15:14:07 +0530

    feat : updated for_loops.py

--- 01_Basics/for_loops.py
+++ 01_Basics/for_loops.py
@@ -2,4 +2,14 @@ nums = [10,20,30,40,50]
 
 for num in nums:
     print(num)
-    
+
+temps = [30,32,35,40,42,28,25]
+total = 0
+count = 0
+for temp in temps:
+    total = temp + total
+    count += 1
+
+average = total/count
+print(f'Average tempreature is {average}')
+
