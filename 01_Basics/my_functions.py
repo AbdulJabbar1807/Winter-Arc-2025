c98b716ac7d725890f13ee679d86b208792aaf69
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2025-12-30 20:40:00 +0530

    feat:updated my_functions.py

--- 01_Basics/my_functions.py
+++ 01_Basics/my_functions.py
@@ -23,13 +23,13 @@ employee_info('Python','Web development',name='AJ',age=27)
 
 #also if we pass the positional arguments as a list and keyword arguments as dicionary,
 # it will interpret them as a single argument.#
-def employee_info(*args,**kwargs):
+def employee_info_(*args,**kwargs):
     print(args)
     print(kwargs)
 
 skills = ['python','web development']
 info = {'name':'AJ','age':'27'}
-employee_info(skills,info)
+employee_info_(skills,info)
 
 # leap year code using python -
 month_days = [0,31,28,31,30,31,30,31,31,30,31,30,31] # 0 at index 0 for placeholder to easily iterate over them 1 - 12 for 12 months.
@@ -51,6 +51,36 @@ def days_in_month(year,month):
 
 print(days_in_month(2024,2))
 
+# ----------------------- #
+#Function example#
+def calculate_average(data:list) -> float :
+    """To calculate the average temperature of 7 days."""
+    total = 0
+    count = 0
+    for temp in data :
+        total = total+temp
+        count += 1
+        
+    if count == 0:
+        return 0.0
+    average = total/count
+    return average
+
+def check_temp(temp : float) -> str : # type: ignore
+    """to check whether the outside temperature is favourable or not."""
+    if temp > 35:
+        return "hot outside."
+    elif temp < 20:
+        return "cold outside."
+    elif 20 <= temp <=35:
+        return "comfortable."
+    
+daily_temp = [35,25,8,45,5,33,28]
+
+print(f"Average is temperature is : {calculate_average(daily_temp)}")
+
+for temp in daily_temp:
+    print(f"Today's weather is {check_temp(temp)}")
 
 
 
