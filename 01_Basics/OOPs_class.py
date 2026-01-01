c707f270fab501fd835889f97b5b69cda9c04b77
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-01-01 18:09:55 +0530

    feat:Add class OOPs

---
+++ 01_Basics/OOPs_class.py
@@ -0,0 +1,14 @@
+class Employee:
+    def __init__(self,first,last,pay) -> None:
+        self.first = first
+        self.last = last
+        self.pay = pay 
+        self.email = first + '.'+last+'@company.com'
+
+emp_1 = Employee('Abdul','Jabbar',500000)
+emp_2 = Employee('Amaan','Khan','600000')
+
+print(emp_1.email)
+print(emp_2.email)
+        
+#Instance contain that data that are unique
