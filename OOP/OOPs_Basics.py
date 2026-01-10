84978fc1ca3c6446133bf30cb7da234ea1503ea3
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-01-10 21:22:33 +0530

    feat: code update.

--- OOP/OOPs_Basics.py
+++ OOP/OOPs_Basics.py
@@ -9,7 +9,9 @@ class Car:
 
 car_1 = Car('Tata','Harrier',2500000)
 car_2 = Car('Hyundai','Creta',200000)
+
 print(f'{car_1.brand} {car_1.model} {car_1.price}')
 print(f'{car_2.brand} {car_2.model} {car_2.price}')
+
 car_1.increase_price(10)
 print(f'Increased price for car 1 {car_1.price}')
