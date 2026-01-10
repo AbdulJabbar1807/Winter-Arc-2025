3fd35681aed4a26d720897d51da72ca1280d92b0
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-01-10 21:21:25 +0530

    feat: code update.

--- OOP/OOPs_Basics.py
+++ OOP/OOPs_Basics.py
@@ -3,8 +3,13 @@ class Car:
         self.brand = brand
         self.model = model
         self.price = price
+    
+    def increase_price(self,percent):
+        self.price = self.price + (self.price*percent/100)
 
 car_1 = Car('Tata','Harrier',2500000)
 car_2 = Car('Hyundai','Creta',200000)
 print(f'{car_1.brand} {car_1.model} {car_1.price}')
 print(f'{car_2.brand} {car_2.model} {car_2.price}')
+car_1.increase_price(10)
+print(f'Increased price for car 1 {car_1.price}')
