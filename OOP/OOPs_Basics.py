2426dfddb1d0a70bcde7a0e609df84ff70d12b32
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-01-11 22:59:15 +0530

    feat : code update.

--- OOP/OOPs_Basics.py
+++ OOP/OOPs_Basics.py
@@ -12,6 +12,15 @@ class Car:
     def price_with_tax(self):
         return self.price + (self.price * Car.tax_rate)
     
+    @classmethod
+    def new_tax_rate(cls,new_rate): # Class method
+        
+        if not isinstance(new_rate,(int,float)):
+            raise ValueError("Tax rate must be a number.")
+        if new_rate < 0 or new_rate > 1:
+            raise ValueError("Tax rate must be between 0 and 1.")
+        
+        cls.tax_rate = new_rate
         
 car_1 = Car('Tata','Harrier',2500000)
 car_2 = Car('Hyundai','Creta',200000)
@@ -31,3 +40,10 @@ Car.tax_rate = 0.30 # tax_rate increased to 0.30,use of class variable.
 
 print(f'Price with increased tax is Rs.{car_1.price_with_tax()} for car 1.')
 print(f'Price with increased tax is Rs.{car_2.price_with_tax()} for car 2.')
+
+Car.new_tax_rate(0.39) # Use of class method
+print(f'New tax rate for car 1 is Rs.{car_1.price_with_tax()}')
+
+
+
+#------------------------------------------------------------------------------------------------------------------#
