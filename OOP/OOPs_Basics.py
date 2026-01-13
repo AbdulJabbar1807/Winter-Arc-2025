85370dceb232693b69e8030ec555f3c99543f8e0
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-01-13 20:05:42 +0530

    feat : code update.

--- OOP/OOPs_Basics.py
+++ OOP/OOPs_Basics.py
@@ -1,57 +1,65 @@
-# class Car:
-#     tax_rate = 0.24 # Class variable (also,24 % of total price).
+class Car:
+    tax_rate = 0.24 # Class variable (also,24 % of total price).
     
-#     def __init__(self, brand, model, price):
-#         Car.is_price_valid(price)
-#         self.brand = brand
-#         self.model = model
-#         self.price = price
+    def __init__(self, brand, model, price):
+        Car.is_price_valid(price)
+        self.brand = brand
+        self.model = model
+        self._price = price # Protected attributes.
     
-#     def increase_price(self,percent): # Instance variable.
-#         self.price = self.price + (self.price*percent/100)
+    def increase_price(self,percent): # Instance variable.
+        self._price = self._price + (self._price*percent/100)
     
-#     def price_with_tax(self): # Use of class variable.
-#         return self.price + (self.price * Car.tax_rate)
+    def price_with_tax(self): # Use of class variable.
+        return self._price + (self._price * Car.tax_rate)
     
-#     @classmethod
-#     def new_tax_rate(cls,new_rate): # Class method
+    @classmethod
+    def new_tax_rate(cls,new_rate): # Class method
         
-#         if not isinstance(new_rate,(int,float)):
-#             raise ValueError("Tax rate must be a number.")
-#         if new_rate < 0 or new_rate > 1:
-#             raise ValueError("Tax rate must be between 0 and 1.")
+        if not isinstance(new_rate,(int,float)):
+            raise ValueError("Tax rate must be a number.")
+        if new_rate < 0 or new_rate > 1:
+            raise ValueError("Tax rate must be between 0 and 1.")
         
-#         cls.tax_rate = new_rate
+        cls.tax_rate = new_rate
         
-#     @staticmethod
-#     def is_price_valid (price):
-#         if not isinstance(price,(int,float)):
-#             raise ValueError("Price should be in number.")
-#         if price <= 0 :
-#             raise ValueError("Price should be greater than zero.")
+    @staticmethod
+    def is_price_valid (price):
+        if not isinstance(price,(int,float)):
+            raise ValueError("Price should be in number.")
+        if price <= 0 :
+            raise ValueError("Price should be greater than zero.")
+        
+    @property
+    def price(self):
+        return self._price
+    @price.setter
+    def price(self,value):
+        Car.is_price_valid(value)
+        self._price = value
     
         
-# car_1 = Car('Tata','Harrier',Car.is_price_valid(2500000))
-# car_2 = Car('Hyundai','Creta',Car.is_price_valid(2000000))
+car_1 = Car('Tata','Harrier',Car.is_price_valid(2500000))
+car_2 = Car('Hyundai','Creta',Car.is_price_valid(2000000))
 
-# print(f'{car_1.brand} {car_1.model} Rs{car_1.price}')
-# print(f'{car_2.brand} {car_2.model} Rs{car_2.price}')
+print(f'{car_1.brand} {car_1.model} Rs{car_1.price}')
+print(f'{car_2.brand} {car_2.model} Rs{car_2.price}')
 
-# car_1.increase_price(10) # Use of Instance variable.
-# print(f'Increased price for car 1 {car_1.price}')
-# car_2.increase_price(20) # Use of Instance variable.
-# print(f'Increased price for car 2 {car_2.price}')
+car_1.increase_price(10) # Use of Instance variable.
+print(f'Increased price for car 1 {car_1.price}')
+car_2.increase_price(20) # Use of Instance variable.
+print(f'Increased price for car 2 {car_2.price}')
 
-# print(f'Price with tax is Rs.{car_1.price_with_tax()} for car 1.')
-# print(f'Price with tax is Rs.{car_2.price_with_tax()} for car 2.')
+print(f'Price with tax is Rs.{car_1.price_with_tax()} for car 1.')
+print(f'Price with tax is Rs.{car_2.price_with_tax()} for car 2.')
 
-# Car.tax_rate = 0.30 # tax_rate increased to 0.30,use of class variable.
+Car.tax_rate = 0.30 # tax_rate increased to 0.30,use of class variable.
 
-# print(f'Price with increased tax is Rs.{car_1.price_with_tax()} for car 1.')
-# print(f'Price with increased tax is Rs.{car_2.price_with_tax()} for car 2.')
+print(f'Price with increased tax is Rs.{car_1.price_with_tax()} for car 1.')
+print(f'Price with increased tax is Rs.{car_2.price_with_tax()} for car 2.')
 
-# Car.new_tax_rate(0.39) # Use of class method
-# print(f'New tax rate for car 1 is Rs.{car_1.price_with_tax()}')
+Car.new_tax_rate(0.39) # Use of class method
+print(f'New tax rate for car 1 is Rs.{car_1.price_with_tax()}')
 
 # # Car.tax_rate = -1 # will give 0.0 .
 # print(car_1.price_with_tax())
@@ -68,7 +76,7 @@ class BankAccount :
         self.account_holder = account_holder
         self.balance = balance
         
-    @staticmethod
+    @staticmethod # Static Method
     def is_balance(balance):
         if not isinstance(balance,(int,float)):
             raise ValueError("Balance should be in number's.")
@@ -77,14 +85,14 @@ class BankAccount :
         self.balance = self.balance + amount
         return self.balance
     
-    def withdraw_amount(self,wd_amount):
+    def withdraw_amount(self,wd_amount): # Instance Method : 2.Withdraw amount
         if wd_amount > self.balance:
             raise ValueError("Insufficient balance.")
         else:
             self.balance = self.balance - wd_amount
         return self.balance
     
-    def check_balance(self):
+    def check_balance(self): # Instance Methos : 3 .Check Balandce
         return self.balance
         
     
--- OOP/README.md
+++ OOP/README.md
@@ -9,3 +9,5 @@
 * **Class Variables:** Class variable's are different from Instance variable's.A Class Variable is shared by all the Instances in the class and are accessible throughout the class.
 * **Methods:** Learnt about the implementation and Difference between Regular,Class,and Static methods in Classes.
 **Class Methods:** This class methods can override the regular methods in the main class without disturbing the whole code.
+**Static Method:** This static methods are method that logically belong to a class but does'nt need access to either the instance(self) or the class(cls).
+**Encapsulation:** The internal state of an object should not be freely modifiable.All changes must go through controlled paths.
