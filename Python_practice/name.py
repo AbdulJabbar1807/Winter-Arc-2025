299bfd0c9617cec49852ffac9c3878ed4fe4e1d0
Abdul Jabbar <abduljabbarkhokhar954@gmail.com>
2026-02-04 17:05:44 +0530

    init:use of // and % in python

--- Python_practice/name.py
+++ Python_practice/name.py
@@ -1,2 +1,10 @@
 from hello_name import name
 name("AJ")
+
+total_seconds = 3685
+hours = total_seconds // (60*60)
+minutes = total_seconds % (60*60) // 60
+seconds = total_seconds % (60*60) % 60
+print(hours)
+print(minutes)
+print(seconds)
